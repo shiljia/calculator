d759bfc4dc8ebb203aea1907319999165851d169
Shiljia M J <113376190+shiljia@users.noreply.github.com>
2023-11-20 12:58:30 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,57 @@
+
+from tkinter import*
+from functools import pratial
+
+root=Tk()
+root.title('calculator')
+
+
+tb=Entry(root,font=('arial',23))
+tb.grid(row=0,column=0,columnspan=4)
+
+def click(t):
+    if(t=='C'):
+        tb.delete(0,END)
+    elif(t=='X'):
+        text.tb.get()[:-1]
+        tb.delete(0,END)
+        tb.insert(0,text)
+    elif(t=='='):
+        ans=eval(tb.get())
+        tb.delete(0,END)
+        tb.insert(0,ans)
+    else:
+        tb.insert(END,t)
+
+btn=['C','(',')','/',
+     '7','8','9','*',
+     '4','5','6','-',
+     '1','2','3','+',
+     'X','0','.','=',]
+
+r=1
+c=0
+clr='gray'
+
+for i in btn:
+    if(i=='C' or i=='X'):
+        clr='red'
+    elif(i=='='):
+        clr='green'
+    else:
+        clr='gray'
+
+    clk=partial(click,i)
+
+    b=button(root,text=1,bg=clr,width=5,height=2,font=('arial',23),command=clk)
+    b.grid(row=r,column=c)
+
+    if(c==3):
+        r+=1
+        c=0
+    else:
+        c+=1
+
+
+
+root.mainloop()
